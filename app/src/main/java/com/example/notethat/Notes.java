c1ed18defc8c55d0f3559c314fb110e92592c4b7
kkgit05 <gamingplacekk@gmail.com>
2025-01-21 17:08:40 +0530

    made popup menu

--- app/src/main/java/com/example/notethat/Notes.java
+++ app/src/main/java/com/example/notethat/Notes.java
@@ -1,13 +1,53 @@
 package com.example.notethat;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Notes extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notes_main);
+
+        Button button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
     }
+
+    private void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+//                    case R.id.action_one:
+//                        // Handle action one
+//                        return true;
+//                    case R.id.action_two:
+//                        // Handle action two
+//                        return true;
+//                    case R.id.action_three:
+//                        // Handle action three
+//                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        popupMenu.show();
+    }
+
 }
